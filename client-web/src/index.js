782c8be7b513d5af3ec6683fd1d578663632a514
beigeelephant <timo@beigeelephant.com>
2017-11-02 14:41:00 +0200

    Removed unnecessary service worker.

--- client-web/src/index.js
+++ client-web/src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore } from "redux";
 import Application from "./Application";
 import { rootReducer } from "./reducers";
-import registerServiceWorker from "./registerServiceWorker";
 import 'semantic-ui-css/semantic.min.css';
 import './styles.css';
 
@@ -13,5 +12,3 @@ ReactDOM.render(
 		<Application />
 	</Provider>,
 	document.getElementById("root"));
-
-registerServiceWorker();
